ec451e303f30a9657134c7e69a257aa17b31025e
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-10-01 14:19:42 -0300

    Alterações na estrutura da tabela
    - Instalado jquery para utilização de AJAX;
    - Criado construtor do componente e criado o estado da aplicação. Inicializado uma lista referente aos autores juntamente dentro do estado;
    - Utilizado função do ReactJS, componentDidMount(), para atualizar a lista de autores após a tela ter sido montada;
    - Refatorado código da montagem das linhas da tabela, para que seja dinâmico, e que monte os dados de acordo com o retorno JSON do servidor.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lista: []
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:8080/api/autores',
+      dataType: 'json',
+      success:function(resposta){
+        this.setState({
+          lista: resposta
+        })
+      }.bind(this)
+    });
+  }
+
   render() {
     return (
       <div id="layout">
@@ -57,10 +77,16 @@ class App extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>Alberto</td>
-                      <td>alberto.souza@caelum.com.br</td>
-                    </tr>
+                    {
+                      this.state.lista.map((autor) => {
+                        return (
+                          <tr key={ autor.id }>
+                            <td>{ autor.nome }</td>
+                            <td>{ autor.email }</td>
+                          </tr>
+                        );
+                      })
+                    }
                   </tbody>
                 </table>
               </div>
