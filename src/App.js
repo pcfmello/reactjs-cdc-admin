5d4c4c2ef739747f1f8a5650c9c4d88e9a1c5897
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-10-01 16:45:36 -0300

    Criação de componentes
    - Criado componente InputCustomizado e colocada toda estrutura de input no mesmo;
    - Criado componente SubmitCustomizado e colocada toda estrutura de submit no mesmo;
    - Criada funções para setar os atributos do formulário;
    - Criada função para enviar dados do formulário para o servidor;
    - Atrelada função de submit no formulário;

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
-
+import InputCustomizado from './componentes/InputCustomizado';
+import SubmitCustomizado from './componentes/SubmitCustomizado';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      lista: []
+      lista: [],
+      nome: '',
+      email: '',
+      senha: ''
     };
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
 
   componentDidMount() {
@@ -21,6 +29,45 @@ class App extends Component {
           lista: resposta
         })
       }.bind(this)
+    });
+  }
+
+  enviaForm(evento){
+    evento.preventDefault();
+    $.ajax({
+      url:'http://localhost:8080/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({
+          nome:this.state.nome,
+          email:this.state.email,
+          senha:this.state.senha
+      }),
+      success: function(resposta){
+        this.setState({lista:resposta});
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({
+      nome: evento.target.value
+    });
+  }
+
+  setEmail(evento){
+    this.setState({
+      email: evento.target.value
+    });
+  }
+
+  setSenha(evento){
+    this.setState({
+      senha: evento.target.value
     });
   }
 
@@ -49,23 +96,18 @@ class App extends Component {
             </div>
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label>
-                    <input id="nome" type="text" name="nome" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value=""  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label>
-                    <input id="senha" type="password" name="senha"  />
-                  </div>
-                  <div className="pure-control-group">
-                    <label></label>
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                  </div>
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <InputCustomizado id="nome" type="text" name="nome"
+                      value={this.state.nome} onChange={this.setNome}
+                      label="Nome"/>
+                  <InputCustomizado id="email" type="text" name="email"
+                      value={this.state.email} onChange={this.setEmail}
+                      label="Email"/>
+                  <InputCustomizado id="senha" type="password" name="senha"
+                      value={this.state.senha} onChange={this.setSenha}
+                      label="Senha"/>
+
+                  <SubmitCustomizado titulo="Enviar" />
                 </form>
               </div>
               <div>
